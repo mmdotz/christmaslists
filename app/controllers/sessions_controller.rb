4ca3452247822d756a667c9fc7eb1f5e1010fcdb
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-13 17:04:39 -0600

    sessions login and logout, add email to user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by_email(params[:email])
+    if @user.present? && @user.authenticate(params[:password])
+      set_user_session(@user)
+      redirect_to home_path, notice: "Successful log-in"
+    else
+      flash[:alert] = "Username or password did not match"
+      render :new
+    end
   end
 
   def destroy
+    session[logged_in_users_id] = nil
+    redirect_to root, notice: "You have logged out"
   end
+
+  def set_user_session(user)
+    session[:logged_in_users_id] = user.id
+  end
+
 end
