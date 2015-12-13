3213cf7c82e325cb8fa250de5b545ba4ac36c589
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-13 11:23:07 -0600

    up locally, root set to login

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
 
   get 'sessions/destroy'
 
+  root "sessions#new"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
