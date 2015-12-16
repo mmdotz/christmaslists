36ff1ebbd67df4d8091599445ca45c2a1c4e47b9
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-16 16:25:48 -0600

    update strong para,s

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -69,6 +69,6 @@ class PurchasesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def purchase_params
-      params.require(:purchase).permit(:item, :url, :vendor, :cost, :tracking_num, :bought, :user_id)
+      params.require(:purchase).permit(:item, :url, :vendor, :cost, :tracking_num, :bought, :user_id, :purchase_date)
     end
 end
