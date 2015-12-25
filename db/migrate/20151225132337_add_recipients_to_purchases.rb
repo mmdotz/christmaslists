c239ea0508a7ecde2098f46b805f7ed333a6193e
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-25 07:27:53 -0600

    removed migration - error

--- db/migrate/20151225132337_add_recipients_to_purchases.rb
+++
@@ -1,6 +0,0 @@
-class AddRecipientsToPurchases < ActiveRecord::Migration
-  def change
-    add_column :purchases, :recipient
-    add_reference :purchases, :recipient, index: true, foreign_key: true
-  end
-end
