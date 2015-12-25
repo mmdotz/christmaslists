18b1e2d2a415b5a2e72ef1d4d2516bd0858b5010
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-25 07:25:43 -0600

    correct migration recip add FK to purchases

--- db/migrate/20151225131204_add_recipient_to_purchases.rb
+++
@@ -1,7 +0,0 @@
-class AddRecipientToPurchases < ActiveRecord::Migration
-  def change
-    add_column :purchases, :recipient
-    add_reference :purchases, :recipient, index: true
-    add_foreign_key :purchases, :recipient
-  end
-end
