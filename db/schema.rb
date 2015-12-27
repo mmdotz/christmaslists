e40e7b24370553586cf0df04fc74ecbca3e468ed
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-27 16:20:07 -0600

    migrate budget

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151226231355) do
+ActiveRecord::Schema.define(version: 20151227221730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "budgets", force: :cascade do |t|
+    t.date     "year"
+    t.integer  "amount"
+    t.string   "user"
+    t.string   "belongs_to"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "clans", force: :cascade do |t|
     t.string   "name"
