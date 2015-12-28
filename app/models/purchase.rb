94a9b29c17b5c846a5d8d10f16b817081790d088
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-28 16:36:47 -0600

    note: user will require nested attributes for budget, test purchase.season attribute before_create new purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,6 +2,11 @@ class Purchase < ActiveRecord::Base
   belongs_to :user
   has_many :recipients
 
+  before_create :set_season
+
+  def set_season
+    self.season = purchase_date.strftime("%Y")
+  end
 
   def season_purchases(user)
       user.purchases
