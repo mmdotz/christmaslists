b433c14889054a92f3c210847b92c7b8034341f3
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-12-28 16:56:45 -0600

    migrate removal of budget from user

--- db/migrate/20151228225044_remove_budget_from_users.rb
+++ db/migrate/20151228225044_remove_budget_from_users.rb
@@ -1,4 +1,5 @@
 class RemoveBudgetFromUsers < ActiveRecord::Migration
   def change
+    remove_column :users, :budget
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228205014) do
+ActiveRecord::Schema.define(version: 20151228225044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,7 +81,6 @@ ActiveRecord::Schema.define(version: 20151228205014) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "email"
-    t.integer  "budget"
   end
 
   create_table "wishlists", force: :cascade do |t|
