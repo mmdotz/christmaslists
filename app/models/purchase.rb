b8d425466dbd6c31c3eb70abf292de26f0f64213
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-01-06 16:22:12 -0600

    corrected to_csv method

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,13 +2,14 @@ class Purchase < ActiveRecord::Base
   belongs_to :user
   has_many :recipients
 
-  self.to_csv
+  def self.to_csv
     attributes = %w()
     CSV.generate(headers:true) do |csv|
       csv << attributes
       all.each do |purchase|
         csv << purchase.attributes.values_at(*attributes)
       end
+    end
   end
 
 
