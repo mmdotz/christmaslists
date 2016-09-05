547c0aee3d828be0b2d046e13feefe852df7f0cc
Michelle Dotzenrod <dotzenrodm@gmail.com>
2016-09-04 20:09:57 -0500

    refactor to get the juices flowing

--- app/helpers/purchases_helper.rb
+++ app/helpers/purchases_helper.rb
@@ -2,14 +2,26 @@ module PurchasesHelper
   # move these to ApplicationHelper and use callback in controller
   def total_number_paid(user) #second argument should be budget year
     user.purchases.paid.count
-    #purchases where purchase_date.year LIKE?, current_year
+    #purchases where purchase_date are between range NOT compared to current_year
   end
+
 
   def total_number_unpaid(user) #second argument should be budget year
     user.purchases.unpaid.count
   end
 
   def total_amt_paid(user) #second argument should be budget year
-    user.purchases.paid.map { |purchase| purchase.cost}.sum
+    user.purchases.paid.pluck(:cost).sum
   end
 end
+
+# 2.2.1 :016 > year
+#  => "2015-07-04 00:00:00"
+# 2.2.1 :017 > require 'date'
+#  => false
+# 2.2.1 :018 > DateTime.strptime(year, "%Y")
+#  => Thu, 01 Jan 2015 00:00:00 +0000
+# 2.2.1 :019 > date = DateTime.strptime(year, "%Y")
+#  => Thu, 01 Jan 2015 00:00:00 +0000
+# 2.2.1 :020 > date.year
+#   2015
